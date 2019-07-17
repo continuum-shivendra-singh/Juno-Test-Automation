9469a259b39915165aac071ae7a71f67dabb9444
shivendra.singh <shivendra.singh@RMM-PN-LT-052.corp.continuum.net>
2019-07-17 11:25:09 +0530

    changes in report js file

--- platform-agent-automation/test-report/cucumber/report.js
+++ platform-agent-automation/test-report/cucumber/report.js
@@ -10,13 +10,7 @@ formatter.feature({
   "name": "Validate Registration, Re-registration, installation and uninstallation of Agent on Windows, Linux, MAC",
   "description": "",
   "id": "validate-registration,-re-registration,-installation-and-uninstallation-of-agent-on-windows,-linux,-mac",
-  "keyword": "Feature",
-  "tags": [
-    {
-      "line": 2,
-      "name": "@registration"
-    }
-  ]
+  "keyword": "Feature"
 });
 formatter.scenario({
   "line": 5,
@@ -24,7 +18,13 @@ formatter.scenario({
   "description": "",
   "id": "validate-registration,-re-registration,-installation-and-uninstallation-of-agent-on-windows,-linux,-mac;verify-agent-is-installated-and-registered-successfully-on-linux",
   "type": "scenario",
-  "keyword": "Scenario"
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 4,
+      "name": "@registration"
+    }
+  ]
 });
 formatter.step({
   "line": 6,
@@ -43,4 +43,23 @@ formatter.step({
 });
 formatter.match({
   "location": "Agent_Registration_Installation.linux_agent_present_on_the_machine()"
+});
+formatter.result({
+  "duration": 2378111500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Agent_Registration_Installation.user_install_agent_on_machine()"
+});
+formatter.result({
+  "duration": 53062602900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Agent_Registration_Installation.agent_install_successfully_on_the_machine()"
+});
+formatter.result({
+  "duration": 306700,
+  "status": "passed"
+});
 });
